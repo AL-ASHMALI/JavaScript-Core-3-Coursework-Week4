e9732b425e1ca9bd2e63724ec37a6535b9d8ce54
Rabie AL-Ashmali <rabiealashmali@gmail.com>
2022-11-10 13:20:58 +0000

    finished all pass-test

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,15 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+    // remove the string from the array
+    let filtered = numbers.filter(number => typeof number === 'number');
+
+    // calculating the average
+    let sum = 0; 
+    filtered.forEach((num) => {sum += num});
+
+    return sum / filtered.length; 
+}
 
 module.exports = average;
--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,16 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+    let totals = {}; 
+
+    carsSold.forEach((car) => {
+        if (car.make !== totals[car.make]) {totals[car.make] = 0}; 
+    }); 
+
+    carsSold.forEach((car) => {
+        (totals[car.make] += car.price); 
+        return totals; 
+    }); 
+
+    return totals;
+}
 
 module.exports = sales;
